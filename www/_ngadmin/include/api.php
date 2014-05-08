95ea2f26902043fdf27d84a34216aaed3bca3ee0
phil <phil@frontside.com.au>
2014-05-08 16:21:52 +1000

    latest

--- www/_ngadmin/include/api.php
+++ www/_ngadmin/include/api.php
@@ -17,9 +17,9 @@ class Api
      * Crud routes
      */
     function setup_routes(){
-     //   echo "<pre>";
-    //    print_r($_SERVER);
-      //  print_r($this->app->request());
+        echo "<pre>";
+        print_r($_SERVER);
+        print_r($this->app->request());
 
         $this->app->get('/',  function(){
             echo "hello slim";
